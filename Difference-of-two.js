7c41dda6382f2a859cd5e712969337eb0c14b94c
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-04 13:14:16 -0800

    Update Difference-of-two.js

--- Difference-of-two.js
+++ Difference-of-two.js
@@ -31,3 +31,14 @@ function twosDifference(input) {
 }
 
 // or
+
+function twosDifference(input) {
+  input.sort((a,b) => a-b)
+  let arr = [];
+  for (let i = 0; i < input.length; ++i) {
+    if (input.indexOf(input[i]+2)!=-1) {
+      arr.push([input[i],input[i]+2])
+    }
+  }
+  return arr;
+}
